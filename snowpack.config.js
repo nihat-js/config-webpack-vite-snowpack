a984d2490cdbcc4a6a3836d8bf0f8f8531f40017
nihat-js <abdullazadenihat@gmail.com>
2023-01-22 13:10:31 +0400

    added config files

---
+++ snowpack.config.js
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+    src : "/public/index.html",
+    public : "/ooo",
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
